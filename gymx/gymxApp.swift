93a7e2877a1bb4577a4f49a7ea6b1bc5d0bf0300
Mustafa Ghazi <ghazimostafa10@icloud.com>
2025-11-05 18:00:23 +0100

    Initial Commit

---
+++ gymx/gymxApp.swift
@@ -0,0 +1,17 @@
+//
+//  gymxApp.swift
+//  gymx
+//
+//  Created by Mustafa Ghazi on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct gymxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
